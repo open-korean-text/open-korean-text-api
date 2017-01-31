70e18e20e70f5a01d9220eaf280e18f26f31aad8
Will Hohyon Ryu <will.ryu@airbnb.com>
2017-01-31 15:40:54 -0800

    Added OpenKoreanText dependencies

--- src/main/java/org/openkoreantext/MyResource.java
+++ src/main/java/org/openkoreantext/MyResource.java
@@ -11,15 +11,15 @@ import javax.ws.rs.core.MediaType;
 @Path("myresource")
 public class MyResource {
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
-    }
+  /**
+   * Method handling HTTP GET requests. The returned object will be sent
+   * to the client as "text/plain" media type.
+   *
+   * @return String that will be returned as a text/plain response.
+   */
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String getIt() {
+    return "Got it!";
+  }
 }
--- src/test/java/org/openkoreantext/MyResourceTest.java
+++ src/test/java/org/openkoreantext/MyResourceTest.java
@@ -5,44 +5,50 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
 import org.glassfish.grizzly.http.server.HttpServer;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openkoreantext.processor.OpenKoreanTextProcessorJava;
+
 import static org.junit.Assert.assertEquals;
 
 public class MyResourceTest {
 
-    private HttpServer server;
-    private WebTarget target;
+  private HttpServer server;
+  private WebTarget target;
 
-    @Before
-    public void setUp() throws Exception {
-        // start the server
-        server = Main.startServer();
-        // create the client
-        Client c = ClientBuilder.newClient();
+  @Before
+  public void setUp() throws Exception {
+    // start the server
+    server = Main.startServer();
+    // create the client
+    Client c = ClientBuilder.newClient();
 
-        // uncomment the following line if you want to enable
-        // support for JSON in the client (you also have to uncomment
-        // dependency on jersey-media-json module in pom.xml and Main.startServer())
-        // --
-        // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
+    // uncomment the following line if you want to enable
+    // support for JSON in the client (you also have to uncomment
+    // dependency on jersey-media-json module in pom.xml and Main.startServer())
+    // --
+    // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
 
-        target = c.target(Main.BASE_URI);
-    }
+    target = c.target(Main.BASE_URI);
+  }
 
-    @After
-    public void tearDown() throws Exception {
-        server.stop();
-    }
+  @After
+  public void tearDown() throws Exception {
+    server.stop();
+  }
 
-    /**
-     * Test to see that the message "Got it!" is sent in the response.
-     */
-    @Test
-    public void testGetIt() {
-        String responseMsg = target.path("myresource").request().get(String.class);
-        assertEquals("Got it!", responseMsg);
-    }
+  /**
+   * Test to see that the message "Got it!" is sent in the response.
+   */
+  @Test
+  public void testGetIt() {
+    String responseMsg = target.path("myresource").request().get(String.class);
+    assertEquals("Got it!", responseMsg);
+  }
+
+  @Test
+  public void testOpenKoreanText() {
+    System.out.println(OpenKoreanTextProcessorJava.tokenize("안녕하세요! 테스트 입니다 "));
+  }
 }
