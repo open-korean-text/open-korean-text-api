c498c9c26f2c2bd935f290b4f5aaf5bb97e5cb8c
Will Hohyon Ryu <will.ryu@airbnb.com>
2017-01-31 20:19:04 -0800

    Lost in Post

--- src/main/java/org/openkoreantext/Processor.java
+++ src/main/java/org/openkoreantext/Processor.java
@@ -1,11 +1,14 @@
 package org.openkoreantext;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import jersey.repackaged.com.google.common.collect.ImmutableMap;
 import org.json.simple.JSONObject;
@@ -27,17 +30,32 @@ public class Processor {
    */
   @GET
   @Produces(MediaType.TEXT_PLAIN)
-  public String processor(
+  public String processorPost(
       @DefaultValue("오픈코리안텍스트") @QueryParam("text") String text
   ) {
+    JSONObject json = tokenizeJson(text);
+
+    return json.toJSONString();
+  }
+
+  @POST
+  @Consumes(MediaType.TEXT_PLAIN)
+  @Produces(MediaType.TEXT_PLAIN)
+  public Response processorGet(
+      @DefaultValue("오픈코리안텍스트") @QueryParam("text") String text
+  ) {
+    JSONObject json = tokenizeJson(text);
+
+    return Response.status(201).entity( json.toJSONString()).build();
+  }
+
+  private JSONObject tokenizeJson(@DefaultValue("오픈코리안텍스트") @QueryParam("text") String text) {
     Seq<KoreanTokenizer.KoreanToken> parsed = OpenKoreanTextProcessorJava.tokenize(text);
 
-    JSONObject json = new JSONObject(
+    return new JSONObject(
         ImmutableMap.of(
             "tokens", OpenKoreanTextProcessorJava.tokensToJavaKoreanTokenList(parsed),
             "token_strings", OpenKoreanTextProcessorJava.tokensToJavaStringList(parsed)
         ));
-
-    return json.toJSONString();
   }
 }
--- src/test/java/org/openkoreantext/ProcessorTest.java
+++ src/test/java/org/openkoreantext/ProcessorTest.java
@@ -1,6 +1,7 @@
 package org.openkoreantext;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.ws.rs.core.Application;
 
@@ -8,6 +9,10 @@ import jersey.repackaged.com.google.common.collect.ImmutableMap;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import static org.junit.Assert.assertEquals;
 
@@ -22,7 +27,7 @@ public class ProcessorTest extends JerseyTest {
    * Test to see that the message "Got it!" is sent in the response.
    */
   @Test
-  public void testProcessor() {
+  public void testProcessorGet() {
     final String responseMsg = target().path("processor").request().get(String.class);
 
     assertEquals(
@@ -32,7 +37,36 @@ public class ProcessorTest extends JerseyTest {
   }
 
   @Test
-  public void testProcessorWithParams() {
+  public void testProcessorGetWithParams() {
+    final String responseMsg = target()
+        .path("processor")
+        .queryParam("text", "한국어처리기입니다.")
+        .request()
+        .get(String.class);
+
+    assertEquals(
+        "{\"tokens\":[한국어(Noun: 0, 3),처리기(ProperNoun: 3, 3),입니(Adjective: 6, 2)," +
+            "다(Eomi: 8, 1),.(Punctuation: 9, 1)]," +
+            "\"token_strings\":[\"한국어\",\"처리기\",\"입니\",\"다\",\".\"]}",
+        responseMsg);
+  }
+
+  @Test
+  public void testProcessorPost() {
+    Map<String, String> data = new HashMap<String, String>();
+    data.put("text", "이게 뭐지?");
+
+    final Response response = target().path("processor").request()
+        .post(Entity.entity(data, MediaType.TEXT_PLAIN_TYPE));
+
+    assertEquals(
+        "{\"tokens\":[오픈(ProperNoun: 0, 2),코리안(ProperNoun: 2, 3),텍스트(Noun: 5, 3)]," +
+            "\"token_strings\":[\"오픈\",\"코리안\",\"텍스트\"]}",
+        response.readEntity(String.class));
+  }
+
+  @Test
+  public void testProcessorPostWithParams() {
     final String responseMsg = target()
         .path("processor")
         .queryParam("text", "한국어처리기입니다.")
