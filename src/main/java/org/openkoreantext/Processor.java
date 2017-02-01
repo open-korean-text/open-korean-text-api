1b67cabd83cb0006694111b2a48e0e7c1da8fcf0
Will Hohyon Ryu <will.ryu@airbnb.com>
2017-01-31 19:49:24 -0800

    Add more tests

--- src/main/java/org/openkoreantext/Processor.java
+++ src/main/java/org/openkoreantext/Processor.java
@@ -1,7 +1,5 @@
 package org.openkoreantext;
 
-import java.util.List;
-
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,8 +7,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-import org.openkoreantext.processor.KoreanTokenJava;
+import jersey.repackaged.com.google.common.collect.ImmutableMap;
+import org.json.simple.JSONObject;
 import org.openkoreantext.processor.OpenKoreanTextProcessorJava;
+import org.openkoreantext.processor.tokenizer.KoreanTokenizer;
+import scala.collection.Seq;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -18,20 +19,24 @@ import org.openkoreantext.processor.OpenKoreanTextProcessorJava;
 @Path("processor")
 public class Processor {
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String processor(
-        @DefaultValue("오픈코리안텍스트") @QueryParam("text") String text
-    ) {
-        List<KoreanTokenJava> parsed =
-            OpenKoreanTextProcessorJava.tokensToJavaKoreanTokenList(
-                OpenKoreanTextProcessorJava.tokenize(text));
-        return parsed.toString();
-    }
+  /**
+   * Method handling HTTP GET requests. The returned object will be sent
+   * to the client as "text/plain" media type.
+   *
+   * @return String that will be returned as a text/plain response.
+   */
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String processor(
+      @DefaultValue("오픈코리안텍스트") @QueryParam("text") String text
+  ) {
+    Seq<KoreanTokenizer.KoreanToken> parsed = OpenKoreanTextProcessorJava.tokenize(text);
+
+    JSONObject json = new JSONObject(ImmutableMap.of(
+        "tokens", OpenKoreanTextProcessorJava.tokensToJavaKoreanTokenList(parsed),
+        "token_strings", OpenKoreanTextProcessorJava.tokensToJavaStringList(parsed)
+    ));
+
+    return json.toJSONString();
+  }
 }
--- src/test/java/org/openkoreantext/ProcessorTest.java
+++ src/test/java/org/openkoreantext/ProcessorTest.java
@@ -1,27 +1,48 @@
 package org.openkoreantext;
 
+import java.util.HashMap;
+
 import javax.ws.rs.core.Application;
 
+import jersey.repackaged.com.google.common.collect.ImmutableMap;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
-
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class ProcessorTest extends JerseyTest {
 
-    @Override
-    protected Application configure() {
-        return new ResourceConfig(Processor.class);
-    }
+  @Override
+  protected Application configure() {
+    return new ResourceConfig(Processor.class);
+  }
 
-    /**
-     * Test to see that the message "Got it!" is sent in the response.
-     */
-    @Test
-    public void testGetIt() {
-        final String responseMsg = target().path("processor").request().get(String.class);
+  /**
+   * Test to see that the message "Got it!" is sent in the response.
+   */
+  @Test
+  public void testProcessor() {
+    final String responseMsg = target().path("processor").request().get(String.class);
 
-        assertEquals("Hello, Heroku!", responseMsg);
-    }
+    assertEquals(
+        "{\"tokens\":[오픈(ProperNoun: 0, 2),코리안(ProperNoun: 2, 3),텍스트(Noun: 5, 3)]," +
+            "\"token_strings\":[\"오픈\",\"코리안\",\"텍스트\"]}",
+        responseMsg);
+  }
+
+  @Test
+  public void testProcessorWithParams() {
+    final String responseMsg = target()
+        .path("processor")
+        .queryParam("text", "한국어처리기입니다.")
+        .request()
+        .get(String.class);
+
+    assertEquals(
+        "{\"tokens\":[한국어(Noun: 0, 3),처리기(ProperNoun: 3, 3),입니(Adjective: 6, 2)," +
+            "다(Eomi: 8, 1),.(Punctuation: 9, 1)]," +
+            "\"token_strings\":[\"한국어\",\"처리기\",\"입니\",\"다\",\".\"]}",
+        responseMsg);
+  }
 }
