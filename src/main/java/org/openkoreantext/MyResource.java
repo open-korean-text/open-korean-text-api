bade4d798ea17bd0e4038aa66fbd301753ccce5c
Will Hohyon Ryu <will.ryu@airbnb.com>
2017-01-31 18:19:17 -0800

    Added open-korean-text

--- src/main/java/org/openkoreantext/MyResource.java
+++ src/main/java/org/openkoreantext/MyResource.java
@@ -1,9 +1,14 @@
 package org.openkoreantext;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.openkoreantext.processor.KoreanTokenJava;
+import org.openkoreantext.processor.OpenKoreanTextProcessorJava;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -20,6 +25,9 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Hello, Heroku!";
+        List<KoreanTokenJava> parsed =
+            OpenKoreanTextProcessorJava.tokensToJavaKoreanTokenList(
+                OpenKoreanTextProcessorJava.tokenize("오픈코리안텍스트 한국어 분석기 API입니다"));
+        return parsed.toString();
     }
 }
